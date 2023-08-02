7282b9ae3572022e3d36b66b2ba4fd883f76739d
Ilmari <ilmari.oivanen@aalto.fi>
2023-08-02 15:17:47 +0300

    works sometimes, spi problem

--- src/main.c
+++ src/main.c
@@ -1,10 +1,8 @@
-/* main.c - Application main entry point */
-
-/*
- * Copyright (c) 2015-2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+#include <stdlib.h>
 
 #include <zephyr/types.h>
 #include <stddef.h>
@@ -14,101 +12,179 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
 
-
-char bt_name[] = "testo";
 #define DEVICE_NAME CONFIG_BT_DEVICE_NAME
-#define DEVICE_NAME_LEN (sizeof(bt_name) - 1)
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 
+static uint8_t mfg_data[] = {"  Table: 1"};
+static int mfg_len = (sizeof(mfg_data) - 1);
 
-/*
- * Set Advertisement data. Based on the Eddystone specification:
- * https://github.com/google/eddystone/blob/master/protocol-specification.md
- * https://github.com/google/eddystone/tree/master/eddystone-url
- */
 static const struct bt_data ad[] = {
-	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-	BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0xaa, 0xfe),
-	BT_DATA_BYTES(BT_DATA_SVC_DATA16,
-		      0xaa, 0xfe, /* Eddystone UUID */
-		      0x10, /* Eddystone-URL frame type */
-		      0x00, /* Calibrated Tx power at 0m */
-		      0x01, /* URL Scheme Prefix https://www. */
-		      't', 'i', 'n', 'y', 'u', 'r', 'l',
-		      0x00, /* .com/ */
-			  '3', 'h', 'm', '4', 'v', 'c', 'k', 'x')
+	BT_DATA(BT_DATA_MANUFACTURER_DATA, mfg_data, 10),
 };
-//https://www.youtube.com/watch?v=y6120QOlsfU -> https://tinyurl.com/3hm4vckx
-
 
 /* Set Scan Response data */
 static const struct bt_data sd[] = {
-	
-	BT_DATA(BT_DATA_NAME_COMPLETE, bt_name, DEVICE_NAME_LEN),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 };
 
-/* Set new Scan Response data */
-static const struct bt_data sd2[] = {
+typedef struct {
+    int eventNumber;
+    int timestamp;
+    float x;
+    float y;
+    float z;
+} SensorEvent;
 
-	BT_DATA(BT_DATA_NAME_COMPLETE, "test2", DEVICE_NAME_LEN),
-};
+static SensorEvent previousEvent;
+float threshold = 1;
+int numEvents = 0;
+bool isFirstEvent = true;
 
-static void bt_ready(int err)
+static void fetch_and_display(const struct device *sensor)
 {
-	char addr_s[BT_ADDR_LE_STR_LEN];
-	bt_addr_le_t addr = {0};
-	size_t count = 1;
+    struct sensor_value accel[3];
+    const char *overrun = "";
+    int rc = sensor_sample_fetch(sensor);
 
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return;
-	}
+    if (rc == -EBADMSG) {
+        /* Sample overrun.  Ignore in polled mode. */
+        if (IS_ENABLED(CONFIG_LIS2DH_TRIGGER)) {
+            overrun = "[OVERRUN] ";
+        }
+        rc = 0;
+    }
+    if (rc == 0) {
+        rc = sensor_channel_get(sensor,
+                                SENSOR_CHAN_ACCEL_XYZ,
+                                accel);
+    }
+    if (rc < 0) {
+        printf("ERROR: Update failed: %d\n", rc);
+    } else {
+        SensorEvent currentEvent;
+        currentEvent.eventNumber = numEvents + 1;
+        currentEvent.timestamp = k_uptime_get_32();
+        currentEvent.x = sensor_value_to_double(&accel[0]);
+        currentEvent.y = sensor_value_to_double(&accel[1]);
+        currentEvent.z = sensor_value_to_double(&accel[2]);
 
-	printk("Bluetooth initialized\n");
+        if (isFirstEvent) {
+            previousEvent = currentEvent;
+            isFirstEvent = false;
+        } else {
+            // Check if the current event is different from the previous event
+            if (abs(currentEvent.x - previousEvent.x) > threshold ||
+                abs(currentEvent.y - previousEvent.y) > threshold ||
+                abs(currentEvent.z - previousEvent.z) > threshold) {
+                numEvents++;
+                previousEvent = currentEvent;
+                printf("Event Number: %d\n", numEvents);
+                printf("Timestamp: %d ms\n", currentEvent.timestamp);
+                printf("x: %d\n", currentEvent.x);
+                printf("y: %d\n", currentEvent.y);
+                printf("z: %d\n", currentEvent.z);
+                printf("\n");
+                
+                printk("Sending advertising data: %02X\n", mfg_data[9]);
+                
+                int err;
+                err = bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+                if (err) {
+                    printk("Advertising failed to start (err %d)\n", err);
+                    return 0;
+                    }
 
+                k_msleep(5000);
 
-	/* Start advertising */
+                err = bt_le_adv_stop();
+                if (err) {
+                    printk("Advertising failed to stop (err %d)\n", err);
+                    return 0;
+                    }
+                else {
+                    printk("Stopping advertising data: %02X\n", mfg_data[9]);
+                }
+                mfg_data[9]++;
+                }
+            }
+        }
+    }
 
-	/* Make a local copy of the BLE advertising parameters */
-	struct bt_le_adv_param adv_params = *(BT_LE_ADV_NCONN_IDENTITY); 
-	adv_params.interval_min = 1000;
-	adv_params.interval_max = 1000;
-	
-	err = bt_le_adv_start(&adv_params, ad, ARRAY_SIZE(ad),
-			      sd, ARRAY_SIZE(sd));
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
-
-
-	/* For connectable advertising you would use
-	 * bt_le_oob_get_local().  For non-connectable non-identity
-	 * advertising an non-resolvable private address is used;
-	 * there is no API to retrieve that.
-	 */
-
-	bt_id_get(&addr, &count);
-	bt_addr_le_to_str(&addr, addr_s, sizeof(addr_s));
-
-	printk("Beacon started, advertising as %s\n", addr_s);
-
-	
-	bt_le_adv_update_data(ad, ARRAY_SIZE(ad),
-			      sd2, ARRAY_SIZE(sd2));
-
-	
+#ifdef CONFIG_LIS2DH_TRIGGER
+static void trigger_handler(const struct device *dev,
+                            const struct sensor_trigger *trig)
+{
+    fetch_and_display(dev);
 }
+#endif
 
 int main(void)
 {
-	int err;
+    const struct device *const sensor = DEVICE_DT_GET_ANY(st_lis2dh);
 
-	printk("Starting Beacon Demo\n");
+    if (sensor == NULL) {
+        printf("No device found\n");
+        return 0;
+    }
+    if (!device_is_ready(sensor)) {
+        printf("Device %s is not ready\n", sensor->name);
+        return 0;
+    }
 
-	/* Initialize the Bluetooth Subsystem */
-	err = bt_enable(bt_ready);
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-	}
-	return 0;
+    // Initialize Bluetooth
+    int err;
+    printk("Starting Broadcaster\n");
+    err = bt_enable(NULL);
+    if (err) {
+        printk("Bluetooth init failed (err %d)\n", err);
+        return 0;
+        }
+    printk("Bluetooth initialized\n");
+
+
+    //k_msleep(1000);
+
+#if CONFIG_LIS2DH_TRIGGER
+    {
+        struct sensor_trigger trig;
+        int rc;
+
+        trig.type = SENSOR_TRIG_DATA_READY;
+        trig.chan = SENSOR_CHAN_ACCEL_XYZ;
+
+        if (IS_ENABLED(CONFIG_LIS2DH_ODR_RUNTIME)) {
+            struct sensor_value odr = {
+                .val1 = 1,
+            };
+
+            rc = sensor_attr_set(sensor, trig.chan,
+                                SENSOR_ATTR_SAMPLING_FREQUENCY,
+                                &odr);
+            if (rc != 0) {
+                printf("Failed to set odr: %d\n", rc);
+                return 0;
+            }
+            printf("Sampling at %u Hz\n", odr.val1);
+        }
+
+        rc = sensor_trigger_set(sensor, &trig, trigger_handler);
+        if (rc != 0) {
+            printf("Failed to set trigger: %d\n", rc);
+            return 0;
+        }
+
+        printf("Waiting for triggers\n");
+        while (true) {
+            k_sleep(K_MSEC(2000));
+        }
+    }
+    
+#else //CONFIG_LIS2DH_TRIGGER
+    printf("Polling at 0.5 Hz\n");
+    while (true) {
+        fetch_and_display(sensor);
+        k_sleep(K_MSEC(1000));
+    }
+
+#endif /* CONFIG_LIS2DH_TRIGGER */
 }
